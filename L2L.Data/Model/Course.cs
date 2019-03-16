5a5a2572624fdbb773147bf0e0387ed54ddbd1bf
Tamas Simon <tom_76@t-online.hu>
2019-03-16 15:36:28 +0100

    #2: EntityFrameworkCore parameters

---
+++ L2L.Data/Model/Course.cs
@@ -0,0 +1,8 @@
+namespace L2L.Data.Model
+{
+    public class Course
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+    }
+}
---
+++ L2L.Data/Model/L2lDbContextFactory.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace L2L.Data.Model
+{
+    public class L2lDbContextFactory : IDesignTimeDbContextFactory<L2lDbContext>
+    {
+        public L2lDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<L2lDbContext>();
+
+            builder.UseSqlite("Data Source=l2l.db;");
+
+            return new L2lDbContext(builder.Options);
+        }
+    }
+
+   
+}
